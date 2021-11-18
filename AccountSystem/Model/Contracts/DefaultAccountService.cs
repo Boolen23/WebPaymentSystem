275cc459e87cc87ed7eb13da0bcc055b6a0c8317
dis3122 <dis3122@gmail.com>
2021-11-18 22:22:14 +0300

    Добавьте файлы проекта.

---
+++ AccountSystem/Model/Contracts/DefaultAccountService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AccountSystem.Model.Contracts
+{
+    public class DefaultAccountService : IAccountService
+    {
+        public string Authorize(string jsonData)
+        {
+            throw new Exception(jsonData);
+        }
+    }
+}
---
+++ AccountSystem/Model/Contracts/IAccountService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Web;
+
+namespace AccountSystem.Model.Contracts
+{
+    [ServiceContract]
+    public interface IAccountService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped)]
+        string Authorize(string jsonData);
+    }
+}
